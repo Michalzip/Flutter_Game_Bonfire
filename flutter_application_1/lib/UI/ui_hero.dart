c6de2d099daf16362c636741747c3b31e66a2609
Michał <63208381+Programmingisfun11@users.noreply.github.com>
2022-04-11 23:37:54 +0200

    Update ui_hero.dart

--- flutter_application_1/lib/UI/ui_hero.dart
+++ flutter_application_1/lib/UI/ui_hero.dart
@@ -22,12 +22,12 @@ drawHealth(canvas);
   
 
   }
-//wszytsko co jest zwiazena z interfcem to gameRef 
+
 
  update(double dt){
 if(gameRef.player!=null){
 
- //przepisz zycie z playera simplePlayer zeby bylo widac jak canvvas sie zmienial
+
   life=gameRef.player?.life ?? 0.0;
 }
 
@@ -38,9 +38,7 @@ if(gameRef.player!=null){
 
 
 void drawHealth(Canvas canvas,){
-  //rysuj linie od do 
 
-  //offety
   double p1=position.left+26;
   double p2=position.top+12;
 canvas.drawLine
@@ -48,10 +46,10 @@ canvas.drawLine
 Offset(p1,p2),
 Offset(p1+life,p2),
 Paint()
-//proporties
+
 ..color=Colors.green[800]!
 ..strokeWidth = strokeWidth
-// ..style = PaintingStyle.fill//nie wiem co to na uczyc sie tegoi i przejzez dokumentacje
+
 
 
 );
