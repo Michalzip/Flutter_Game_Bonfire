21f7dcce55c54643b8436fcdf06c15597488639f
Michał <63208381+Programmingisfun11@users.noreply.github.com>
2022-06-12 21:48:19 +0200

    Update main.dart

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -1,21 +1,11 @@
 
-// import 'we.dart';
-// import 'package:bonfire/bonfire.dart';
 import 'package:flutter/material.dart';
 import 'util/sound.dart';
 import 'startGame.dart';
-// import 'package:flutter/foundation.dart';
+
 void main() async {
-  //  WidgetsFlutterBinding.ensureInitialized();
-//  if (!kIsWeb) {
-//     await Flame.device.setLandscape();
-//     await Flame.device.fullScreen();
-//   }
-  // final myGame = AudioGame();
-//await Audio.initialize(); 
-
+  
   runApp( 
-
  MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
