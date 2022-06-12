0559d12dcbc892a2477c3c5ed6c720b19d8fef3b
Michał <63208381+Programmingisfun11@users.noreply.github.com>
2022-06-12 21:48:43 +0200

    Update light.dart

--- flutter_application_1/lib/light.dart
+++ flutter_application_1/lib/light.dart
@@ -14,15 +14,9 @@ LighttTorch1(Vector2 position):super.withSprite(Sprite.load("torch.png"), positi
 {
   setupLighting(LightingConfig(radius: width * 1.5 , color:  Colors.deepOrangeAccent.withOpacity(0.3),withPulse: true));
 
-
 }
-
-
- 
 }
 class LighttTorch2 extends GameDecoration with Lighting {
-
-
 LighttTorch2(Vector2 position):super.withAnimation(SpriteAnimation.load("torch2.png",SpriteAnimationData.sequenced(amount: 3, stepTime: 0.1, textureSize: Vector2(8,8))), position: position, height: 40, width: 40)
 {
 
@@ -31,10 +25,7 @@ LighttTorch2(Vector2 position):super.withAnimation(SpriteAnimation.load("torch2.
     pulseVariation: 0.1,
    withPulse :true, 
    
-   //pulseCurve: Curves.easeInOutSine,
-  //  color:  Colors.transparent,
-   // pulseVariation: 0.1,
-        color: Colors.deepOrangeAccent.withOpacity(0.3),
+    color: Colors.deepOrangeAccent.withOpacity(0.3),
    ));
 }
 
