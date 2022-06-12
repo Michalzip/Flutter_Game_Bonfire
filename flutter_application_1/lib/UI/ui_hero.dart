03598d6189933cb0751849215e7fa4ad42e53108
Michał <63208381+Programmingisfun11@users.noreply.github.com>
2022-06-12 21:47:29 +0200

    Update ui_hero.dart

--- flutter_application_1/lib/UI/ui_hero.dart
+++ flutter_application_1/lib/UI/ui_hero.dart
@@ -11,51 +11,32 @@ class HealthBar extends InterfaceComponent
   HealthBar():super(id: 1,sprite:Sprite.load("health_ui.png") ,height:26,width:130,position:Vector2(20,20));
 
 
-
 @override
-
- render(Canvas canvas){
+ render(Canvas canvas) {
 super.render(canvas);
-
-
 drawHealth(canvas);
-  
-
-  }
+ }
 
 
  update(double dt){
 if(gameRef.player!=null){
-
-
   life=gameRef.player?.life ?? 0.0;
 }
-
  super.update(dt);
 }
 
-
-
-
 void drawHealth(Canvas canvas,){
-
   double p1=position.left+26;
   double p2=position.top+12;
-canvas.drawLine
-(
+  
+canvas.drawLine(
 Offset(p1,p2),
 Offset(p1+life,p2),
 Paint()
-
 ..color=Colors.green[800]!
 ..strokeWidth = strokeWidth
-
-
-
+  
 );
-
 }
-
-
 }
 
